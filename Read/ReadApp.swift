86433ef6e3536745d675e40339c9e7c58459fbe7
Marco S Hyman <marc@snafu.org>
2023-11-06 14:16:08 -0800

    Initial Commit

---
+++ Read/ReadApp.swift
@@ -0,0 +1,32 @@
+//
+//  ReadApp.swift
+//  Read
+//
+//  Created by Marco S Hyman on 11/6/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ReadApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
