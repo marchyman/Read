3bd20a77708e2593a1a39a07f7b8873264b4cb76
Marco S Hyman <marc@snafu.org>
2025-07-02 20:36:57 -0700

    Move Assets and entitlements to a Supporting Files folder
    Add a stub ReducerTest and a ReadTests folder for unit tests that need
    to be written to check the store/state/reducer that also need to be
    written.

---
+++ ReadTests/ReducerTests.swift
@@ -0,0 +1,18 @@
+//
+// Copyright 2025 Marco S Hyman
+// https://www.snafu.org/
+//
+
+import Foundation
+import SwiftData
+import Testing
+import UDF
+
+@testable import Read
+
+@MainActor
+struct ReducerTests {
+    @Test func someTest() async throws {
+        // ...
+    }
+}
